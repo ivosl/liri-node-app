d5b9898d42a8105ab776249884f3e7c9c529a026
Ivo <ivol@Ivos-MacBook-Air.local>
2018-02-12 20:40:13 -0600

    commiting all files

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.iIoDPVRlx7TEx7SvJ0TPgD5e1,
+  consumer_secret: process.env.bAYp7Prnhza179i4jwasFYVZAkp51xOxMifWw2x8LIn50I4cBQ,
+  access_token_key: process.env.963139971543683072-GJqfM7ADZkoa0YvboPCriTYDJ6q0NNw,
+  access_token_secret: process.env.SUoOpice7lM1sKVtg7IQXArrcWbbiTKqlnxLIwZScL53q
+};
+
+exports.spotify = {
+    id: process.env.652d6145953c4553acba82f3b4af3648,
+    secret: process.env.39da484871a64d52887d2f129cc747f2
+  };
