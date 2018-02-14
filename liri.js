fd83e1d952d22757587ea131441cf5d2f1b45f97
Ivo <ivol@Ivos-MacBook-Air.local>
2018-02-14 17:22:15 -0600

    spotify

--- liri.js
+++ liri.js
@@ -11,41 +11,67 @@ var argument1 = process.argv[2];
 var argument2 = process.argv[3];
 
 //---------------------------------------Twitter-----------------------------
-// var Twitter = require("twitter");
-// var client = new Twitter(keys.twitter);
+var Twitter = require("twitter");
+var client = new Twitter(keys.twitter);
 
-// console.log("My last tweets:");
-// // console.log(client);
-// console.log("-------------------------");
+// console.log(client);
+console.log("-------------------------");
 
-// var params = {ivo_johnny: 'nodejs'};
-// if (argument1 === "my-tweets"){
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//     if (!error) {
-//         for (var i = 0; i < tweets.length; i++){
-//         console.log(tweets[i].text)
-//     return}
-//     } else {
-//         console.log(error);
-//     }
-// });
-// }
+var params = {ivo_johnny: 'nodejs'};
+if (argument1 === "my-tweets"){
+    console.log("My last tweets:");
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+        for (var i = 0; i < tweets.length && i < 20; i++) {
+        console.log(tweets[i].text + "  " + tweets[i].created_at);
+        }
+    } else {
+        console.log(error);
+    }
+});
+}
 //-----------------------------------Spotify----------------------------------
 
-// var Spotify = require('node-spotify-api');
-// var spotify = new Spotify(keys.spotify);
-// console.log("My Music:");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+console.log("My Music:");
 // console.log(keys.spotify);
 
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//     if (err) {
-//         return console.log('Error occurred: ' + err);
-//     }
-    
-//     console.log(data); 
-// });
+    if (argument1 === "spotify-this-song"){
 
+        var songTitle = process.argv;
 
+        var songName = "";
+
+        for (var i = 3; i < songTitle.length; i++) {
+
+            if (i > 3 && i < songTitle.length){
+                songName = songName + "+" + songTitle[i];
+            } else {
+                songName += songTitle[i];
+            }
+        }
+        if (argument1 === "spotify-this-song" && argument2 === undefined){
+            console.log("HERE")
+            songName = "The Sign Ace of Base";
+        }
+
+spotify.search({ type: 'track', query: songName, limit: 3 }, function(err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+
+    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    // for (var k = 0; k < data.tracks.items.length; k++) { 
+    // console.log("Artist: " + data.tracks.items[k].artists[0].name  + "\nSong's Name: " + data.tracks.items[k].name + "\nPreview link: " + data.tracks.items[k].preview_url + "\nAlbum: " + data.tracks.items[k].album.name); 
+    // } 
+
+//  console.log(data); 
+//  console.log(data.tracks);
+console.log("Artist: " + data.tracks.items[0].artists[0].name  + "\nSong's Name: " + data.tracks.items[0].name + "\nPreview link: " + data.tracks.items[0].preview_url + "\nAlbum: " + data.tracks.items[0].album.name);
+});
+    }
+   
 
 // -------------------------------------------- OMBD -----------------------------------
 
@@ -74,13 +100,16 @@ if (argument1 === "movie-this") {
             
         }
     }
-}
+
 //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.
 //Here I check if the user requests a movie information but leaves argument 2  blank
-if (argument1 === "movie-this" && argument2 === ""){
+console.log("argument 1: ", argument1)
+console.log("argument 2: ", argument2)
+if (argument1 === "movie-this" && argument2 === undefined){
+    console.log("HERE")
     movieName = "Mr. Nobody";
 }
-
+console.log(movieName);
 // Then run a request to the OMDB API with the movie specified
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
@@ -102,7 +131,7 @@ request(queryUrl, function(error, response, body) {
         console.log("Actors in the movie: " + JSON.parse(body).Actors);
     }
 });
-
+}
 
 
 
